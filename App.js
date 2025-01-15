000db8a8e628f6ab725496a560f0475056500834
yassineelmiri <yassireelhima@gmail.com>
2025-01-15 09:47:16 +0100

    Created main application file App.js

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { onAuthStateChanged } from 'firebase/auth';
+import Login from './app/screens/Login';
+import List from './app/screens/List';
+import Details from './app/screens/Details';
+import AddAdvertisement from './app/screens/AddAdvertisement';
+import { FIREBASE_AUTH } from './Firebase.config';
 
-export default function App() {
+const Stack = createStackNavigator();
+const InsideStack = createStackNavigator();
+
+function InsideLayout() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <InsideStack.Navigator>
+      <InsideStack.Screen
+        name="List "
+        component={List}
+        options={{ headerShown: true, title: 'Publications' }}
+      />
+  <InsideStack.Screen
+    name="Details"
+    component={Details}
+    options={{ title: 'Détails' }}
+/>
+      <InsideStack.Screen
+        name="AddAdvertisement"
+        component={AddAdvertisement}
+        options={{ title: 'Ajouter une publicité' }}
+      />
+    </InsideStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (currentUser) => {
+      console.log('user', currentUser);
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator initialRouteName="Login">
+        {user ? (
+          <Stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  );
+}
